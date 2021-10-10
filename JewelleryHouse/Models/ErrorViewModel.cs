6a75747931c5c9723c4db7aa06d80372342467fb
sahil <sahil@DESKTOP-GTMMKQM>
2021-10-11 06:04:04 +1300

    Add project files.

---
+++ JewelleryHouse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JewelleryHouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
